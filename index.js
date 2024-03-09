b331b407793cc11c825ad266f48c36f8d5bff817
Safiullah-97 <Safiullah.cap@gmail.com>
2024-03-09 19:42:30 +0400

    index file change

--- index.js
+++ index.js
@@ -1,40 +1,35 @@
-require("dotenv").config();
-const mongoose = require("mongoose");
 const express = require("express");
-const DB_NAME = require("./constants");
-const router= require('./routes/app')
+const mongoose = require("mongoose");
+const router = require('./routes/app');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require("dotenv").config();
+
 const app = express();
-const bodyParser= require('body-parser')
-var cors = require('cors')
-app.use(express.json())
 
-var corsOptions = {
-  origin:'https://lmsfront.netlify.app/api/v1/books',
-  // origin:'http://localhost:3000/api/v1/books',
-  optionsSuccessStatus: 200 
-}
+// Enable CORS for specific origin
+const corsOptions = {
+  origin: 'https://lmsfront.netlify.app', // Adjust the origin to match your frontend URL
+  optionsSuccessStatus: 200
+};
 
-// 'https://library-management-system-mern-stack.vercel.app/'
+// Use CORS middleware with specific options
+app.use(cors(corsOptions));
 
-app.use(cors())
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.get("/", (req, res)=>{
-//   res.json("Hii")
-// })
-
-// app.use(bodyParser)
+// Routes
 app.use('/api/v1/books', router);
 
-
-try {
-  const db = mongoose.connect(`${process.env.MONGODB_URI}`);
-  console.log("Database Connected!");
-
-  app.listen(process.env.PORT, () => {
-    
-    console.log(`The Server is Running on:${process.env.PORT}`);
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Database Connected!");
+    app.listen(process.env.PORT, () => {
+      console.log(`The Server is Running on:${process.env.PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error("Database Cannot Connect!", err);
   });
-} catch (err) {
-  console.log("Database Cannot Connect!", err);
-}
-
